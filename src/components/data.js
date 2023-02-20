8b68923028f116af8ab6b714f9c4587aa58d016e
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-02-20 08:39:42 +0300

    first commit

---
+++ src/components/data.js
@@ -0,0 +1,44 @@
+export const values = [
+  {
+    value: 9,
+  },
+  {
+    value: 8,
+  },
+  {
+    value: 7,
+  },
+  {
+    value: "/",
+  },
+  {
+    value: 6,
+  },
+  {
+    value: 5,
+  },
+  {
+    value: 4,
+  },
+  {
+    value: "X",
+  },
+  {
+    value: 3,
+  },
+  {
+    value: 2,
+  },
+  {
+    value: 1,
+  },
+  {
+    value: "+",
+  },
+  {
+    value: 0,
+  },
+  {
+    value: "-",
+  },
+];
